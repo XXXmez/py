0a9498a176c0cb37b3727fbe1782881e2d539fbb
Grymyl <russian.bogd@gmail.com>
2020-12-28 16:16:02 +0300

    Finished the second chapter

--- python_work/hello_world.py
+++ python_work/hello_world.py
@@ -1,8 +1,86 @@
 #Начало
-message = "Hello Puthon world!"
-print (message)
+message_Hello_p = "Hello Puthon world!"
+print (message_Hello_p)
 
-message = "Hello Python Crash Course world!"
-print(message)
+message_Hello_p = "Hello Python Crash Course world!"
+print(message_Hello_p)
 
 #Строки
+name_ada = "ada lovelace"
+print(name_ada.title()) #Ada Lovelace
+print(name_ada.upper()) #ADA LOVELACE
+print(name_ada.lower()) #ada lovelace
+
+#Конкатенация
+first_name_ada = "ada"
+last_name_ada = "lovelace"
+full_name_ada = first_name_ada + " " + last_name_ada
+print(full_name_ada)
+print("Hello, " + full_name_ada.title() + "!") #Hello, Ada Lovelace!
+massage_ada_lovelace = "Hello, " + full_name_ada.title() + "!"
+print(massage_ada_lovelace) #Hello, Ada Lovelace!
+
+#Табуляции и разрывы строк
+print("\tHello\tworld") #	Hello	world
+print("Hello\nworld") #Hello
+#                      world
+print("1 one \n\t1.1 two\n\t1.2 free\n\t1.3 for")
+
+#Удаление пропусков
+delete_prop_one = "            Ruslan            "
+delete_prop_right = delete_prop_one.rstrip()
+delete_prop_left = delete_prop_one.lstrip()
+delete_prop_two = delete_prop_one.strip()
+print(delete_prop_right)
+print(delete_prop_left)
+print(delete_prop_two)
+
+#Предотвращение синтаксических ошибок в строках
+message_one_of_pyt_1 = "One of Python's strengths is its diverse community."
+print(message_one_of_pyt_1)
+#message_one_of_pyt_2 = 'One of Python's strengths is its diverse community.'
+#print(message_one_of_pyt_2)   #SyntaxError: invalid syntax
+
+
+#Числа
+#Целые числа
+#целыми числами можно выполнять операции
+print(2 + 6)
+print(6 - 2)
+print(6 / 2)
+#операции возведения в степень
+print(2 ** 6)
+#Python также существует определенный порядок операций
+print(2 + 2 * 2)
+print((2 + 2) * 2)
+
+#Вещественные числа
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+#Однако
+print(0.2 + 0.1) #0.30000000000000004
+print(3 * 0.1)   #0.30000000000000004
+
+
+#Предотвращение ошибок типов с использованием функции str()
+age_error_str = 23
+message_error_str = "Happy " + str(age_error_str) + "rd Birthday!"
+print (message_error_str)
+
+#деление
+print(3/2)   #1.5
+print(3.0/2)   #1.5
+print (3/2.0)   #1.5
+print(3.0/2.0)   #1.5
+
+#Комментарии
+# Say hello to everyone.
+print("Hello Python people!")
+
+#Философия Python
+import this
+
+#конец 2 главы
+#начало 3 главы
---
+++ python_work/name_cases.py
@@ -0,0 +1,53 @@
+#УПРАЖНЕНИЯ
+#2-3
+name_2_3 = "Eric"
+message_2_3 = "Hello " + name_2_3 + ", would you like to learn some Python today?"
+print (message_2_3)
+
+#2-4
+name_2_4 = "Eric"
+message_2_4 = "Hello " + name_2_4 + ", would you like to learn some Python today?"
+print (name_2_4.lower()) #eric
+print(name_2_4.upper())  #ERIC
+print(message_2_4.title())
+
+#2-5
+author_quote = "Albert Einstein"
+author_text = author_quote + " once said, \"A person who never made a mistake never tried anything new.\""
+print(author_text)
+
+#2-6
+famous_person = "Albert Einstein"
+author_text = famous_person + " once said, \"A person who never made a mistake never tried anything new.\""
+print(author_text)
+
+#2-7
+name_2_7 = "  R\tusl\nan  "
+print(name_2_7)
+print(name_2_7.lstrip())
+print(name_2_7.rstrip())
+print(name_2_7.strip())
+
+
+#УПРАЖНЕНИЯ
+#2-8
+print(7 + 1)
+print(10 - 2)
+print(2 * 2 * 2)
+print(16 / 2)
+
+#2-9
+love_namber = 12
+love_namber_text = "My loves number this is " + str(love_namber)
+print(love_namber_text) # My loves number this is 12
+
+#УПРАЖНЕНИЯ
+#2-10
+#Добавление комментариев - автоматическое выполнение =)
+
+#УПРАЖНЕНИЯ
+#2-11.
+import this
+
+#конец 2 главы
+#начало 3 главы
